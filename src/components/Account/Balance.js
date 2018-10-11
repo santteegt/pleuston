ee23f18c281af6e9e4f90e1f612fdb076b48aaae
Matthias Kretschmann <m@kretschmann.io>
2018-10-11 13:34:47 +0200

    show longer Ether value in tooltip

--- src/components/Account/Balance.js
+++ src/components/Account/Balance.js
@@ -2,16 +2,20 @@ import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import styles from './Balance.module.scss'
 
-const Balance = ({ eth, ocn }) => (
-    <Fragment>
-        <span className={styles.balance}>
-            <strong>{(eth / 1e18).toFixed(2)}</strong> Ξ
-        </span>
-        <span className={styles.balance}>
-            <strong>{ocn}</strong> Ọ
-        </span>
-    </Fragment>
-)
+const Balance = ({ eth, ocn }) => {
+    const ethFromWei = (eth / 1e18)
+
+    return (
+        <Fragment>
+            <span className={styles.balance} title={ethFromWei.toFixed(10)}>
+                <strong>{ethFromWei.toFixed(2)}</strong> Ξ
+            </span>
+            <span className={styles.balance}>
+                <strong>{ocn}</strong> Ọ
+            </span>
+        </Fragment>
+    )
+}
 
 Balance.propTypes = {
     eth: PropTypes.string.isRequired,
