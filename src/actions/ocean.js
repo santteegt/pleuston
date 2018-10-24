cde8a79eff9b5b8fb48b19b2af5dc05392393bbe
Matthias Kretschmann <m@kretschmann.io>
2018-10-24 15:35:35 +0200

    Provider -> Aquarius

--- src/actions/ocean.js
+++ src/actions/ocean.js
@@ -6,21 +6,21 @@ import {
     keeperScheme,
     keeperHost,
     keeperPort,
-    providerScheme,
-    providerHost,
-    providerPort
+    aquariusScheme,
+    aquariusHost,
+    aquariusPort
 } from '../../config/ocean'
 
 export async function provideOcean() {
     const nodeUri = `${keeperScheme}://${keeperHost}:${keeperPort}`
-    const providerUri = `${providerScheme}://${providerHost}:${providerPort}/api/v1/provider`
+    const aquariusUri = `${aquariusScheme}://${aquariusHost}:${aquariusPort}/api/v1/provider`
 
     const config = {
         // todo: change this when the new interface of metmask is released
         web3Provider: global.web3 ? global.web3.currentProvider : null,
         // this is just a fallback in case web3 is not injected
         nodeUri,
-        providerUri
+        aquariusUri
     }
 
     const ocean = Ocean.getInstance ? await Ocean.getInstance(config) : await new Ocean(config)
