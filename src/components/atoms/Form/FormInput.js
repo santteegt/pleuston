515b1aefaa5059a79b7fd9e521be1677e63ce4b8
Matthias Kretschmann <m@kretschmann.io>
2018-11-05 15:59:25 +0100

    search input tweaks

--- src/components/atoms/Form/FormInput.js
+++ src/components/atoms/Form/FormInput.js
@@ -1,15 +1,28 @@
-import React, { Fragment, PureComponent } from 'react'
+import React, { PureComponent } from 'react'
 import { Field } from 'redux-form'
+import { ReactComponent as SearchIcon } from '../../../svg/search.svg'
 import './FormInput.scss'
 
 class FormInput extends PureComponent {
     state = { isFocused: false }
 
+    inputWrapClasses() {
+        if (this.props.type === 'search') {
+            return 'input-wrap input-wrap-search'
+        } else if (this.props.type === 'search' && this.state.isFocused) {
+            return 'input-wrap input-wrap-search is-focused'
+        } else if (this.state.isFocused && this.props.type !== 'search') {
+            return 'input-wrap is-focused'
+        } else {
+            return 'input-wrap'
+        }
+    }
+
     render() {
-        const { name, label, required } = this.props
+        const { name, label, required, type } = this.props
 
         return (
-            <Fragment>
+            <>
                 <label
                     htmlFor={name}
                     className={required ? 'form__label is-required' : 'form__label'}
@@ -17,7 +30,7 @@ class FormInput extends PureComponent {
                 >
                     {label}
                 </label>
-                <div className={this.state.isFocused ? 'input-wrap is-focused' : 'input-wrap'}>
+                <div className={this.inputWrapClasses()}>
                     <Field
                         className="input"
                         id={name}
@@ -25,8 +38,9 @@ class FormInput extends PureComponent {
                         onFocus={() => this.setState({ isFocused: true })}
                         onBlur={() => this.setState({ isFocused: false })}
                     />
+                    {type === 'search' && <SearchIcon />}
                 </div>
-            </Fragment>
+            </>
         )
     }
 }
