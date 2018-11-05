4f1ea310f608be7415ae3d680b4992384c660eb6
Jernej Pregelj <jernej.pregelj88@gmail.com>
2018-11-05 13:53:14 +0100

    search ui

--- src/components/asset/AssetSearch.js
+++ src/components/asset/AssetSearch.js
@@ -3,30 +3,41 @@ import PropTypes from 'prop-types'
 import { reduxForm } from 'redux-form'
 import Button from '../atoms/Button'
 import FormInput from '../atoms/Form/FormInput'
-import FormHelp from '../atoms/Form/FormHelp'
-import AssetListLoader from '../../containers/AssetListLoader'
+import AssetList from '../../components/asset/AssetList'
+import ScreenHeader from '../../components/ScreenHeader'
 
-const AssetSearch = ({ page, handleSubmit }) => (
+import styles from './AssetSearch.module.scss'
+
+const AssetSearch = ({ page, handleSubmit, assets, handleClick, handleSetPage }) => (
     <Fragment>
-        <form className="form" onSubmit={handleSubmit}>
-            <div className="form__group">
-                <FormInput label="Search" name="text" required component="input" type="text" placeholder="" />
-                <FormHelp>Search for any keyword in the name or description.</FormHelp>
-            </div>
-            <div className="form__group">
-                <Button primary="true" type="submit">Search</Button>
-            </div>
-        </form>
-        <AssetListLoader />
-        <div>
-          TODO PAGINATION
+        <div className={styles.searchHolder}>
+            <form onSubmit={handleSubmit} className={styles.search}>
+                <ScreenHeader title="Data Sets" subtitle="Explore all data sets" />
+                <div className="form__group">
+                    <FormInput label="Search" name="text" component="input" type="text" placeholder="Search for keyword in the name or description." />
+                </div>
+                <div className="form__group">
+                    <Button primary="true" type="submit">Search</Button>
+                </div>
+            </form>
         </div>
+        <AssetList assets={assets} handleClick={handleClick} />
+        {assets && assets.length && (
+            <div className={styles.pagination}>
+                <a href="#" className={styles.item} onClick={() => handleSetPage(page - 1)}>&lt; Prev page</a>
+                <span className={styles.item}>{ page }</span>
+                <a href="#" className={styles.item} onClick={() => handleSetPage(page + 1)}>Next page &gt;</a>
+            </div>
+        )}
     </Fragment>
 )
 
 AssetSearch.propTypes = {
     handleSubmit: PropTypes.func.isRequired,
-    page: PropTypes.number.isRequired
+    handleClick: PropTypes.func.isRequired,
+    page: PropTypes.number.isRequired,
+    handleSetPage: PropTypes.func.isRequired,
+    assets: PropTypes.array.isRequired
 }
 
 const AssetSearchForm = reduxForm({
--- src/containers/AssetSearchLoader.js
+++ src/containers/AssetSearchLoader.js
@@ -1,17 +1,27 @@
 import { connect } from 'react-redux'
+import { push } from 'connected-react-router'
 
 import AssetSearch from '../components/asset/AssetSearch'
-import { getAssets, setAssetSearch } from '../actions'
+import { getAssets, setAssetSearch, setActiveAsset, setAssetSearchPage } from '../actions'
 
 export default connect(
     state => ({
         initialValues: state.asset.search,
-        page: state.asset.search.page
+        page: state.asset.search.page,
+        assets: Object.values(state.asset.assets)
     }),
 
     dispatch => ({
         onSubmit: values => {
             dispatch(setAssetSearch(values))
+            dispatch(getAssets())
+        },
+        handleClick: asset => {
+            dispatch(setActiveAsset(asset.assetId))
+            dispatch(push(`/${asset.assetId}`))
+        },
+        handleSetPage: page => {
+            dispatch(setAssetSearchPage(page))
             dispatch(getAssets())
         }
     })
