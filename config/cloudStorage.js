f1a1fdfae893dc15204e574a98db544d37c4958b
Jernej Pregelj <jernej.pregelj88@gmail.com>
2018-12-07 14:14:38 +0100

    fix access key

--- config/cloudStorage.js
+++ config/cloudStorage.js
@@ -1,6 +1,6 @@
 module.exports = {
     storageAccount: 'testocnfiles',
-    accessKey: 'DCNmb542DWbtkPf1lKz+WXii6Z50vScBvVJQMOy4XtG+bVIjHymbKm8iUZnSdlQlLRsrxlWaOwvAzbQmSm/oBw==',
+    accessKey: 's1wQNLo/on+vMcl20YuQGUY3BUU4+xcGqspiq4OXzdKy6FqfrR+MaIyWf1m1sWBxdbr0DommImHTi3alfQxprQ==',
     appId: '86351e3e-55f0-4b6e-9eab-d0516846ce19',
     redirectHost: 'http://localhost:3000',
     scope: 'https://storage.azure.com/user_impersonation'
