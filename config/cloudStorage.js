ef43b67e93a59df1ea849e6a83fee982d4c55b61
Jernej Pregelj <jernej.pregelj88@gmail.com>
2019-01-15 15:57:09 +0100

    remove conf

--- config/cloudStorage.js
+++ config/cloudStorage.js
@@ -1,7 +1,7 @@
 module.exports = {
     storageAccount: 'testocnfiles',
-    accessKey: 's1wQNLo/on+vMcl20YuQGUY3BUU4+xcGqspiq4OXzdKy6FqfrR+MaIyWf1m1sWBxdbr0DommImHTi3alfQxprQ==',
-    appId: '86351e3e-55f0-4b6e-9eab-d0516846ce19',
+    accessKey: '',
+    appId: '',
     redirectHost: 'http://localhost:3000',
     scope: 'https://storage.azure.com/user_impersonation'
 }
